d60fa6653c46ad4c74c3d3fd0e2875090a308e86
SandipSaeculum26 <sandeep.yadav@saeculumsolutions.com>
2025-06-09 19:00:44 +0530

    learning about the dom

---
+++ javaScript/DOM/script.js
@@ -0,0 +1,39 @@
+// function sayHi() {
+//   document.body.children[4].src =
+//     "https://cdn.pixabay.com/photo/2014/02/27/16/10/flowers-276014__340.jpg";
+// }
+
+// document.body.children[0].innerHtml = "Sandip";
+
+// document.body.children[4].innerHtml =
+//   "hii";
+
+// document.body.children[2].innerHTML = "Sandip";
+
+// document.body.children[3].innerHTML = "yadav";
+
+
+// const cssImage = document.body.children[4]
+
+// console.log(cssImage);
+
+// cssImage.src = 'https://cdn.vectorstock.com/i/1000v/27/74/vanilla-javascript-language-vector-31602774.jpg'
+
+
+
+// -----------------------------------------------------------
+
+// all tags are return in the form of array
+// document.getElementsByTagName("p")
+
+
+
+// all Classes are return in the form of array
+// document.getElementsByClassName("p");
+
+
+
+// all Ids are return in the form of array
+// document.getElement ById("p");
+
+
